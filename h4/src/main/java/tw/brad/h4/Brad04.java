26971fd50446b73ea11a697a2120cd56a0e50d3f
User <User@B01.r1805.tciii.org.tw>
2024-12-17 10:33:57 +0800

    12171033

--- h4/src/main/java/tw/brad/h4/Brad04.java
+++ h4/src/main/java/tw/brad/h4/Brad04.java
@@ -7,24 +7,35 @@ import tw.brad.model.Info;
 import tw.brad.model.Member;
 import tw.brad.utils.HibernateUtil;
 
-public class Brad03 {
+public class Brad04 {
 
 	public static void main(String[] args) {
 		Transaction transaction = null;
 		
 		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
 
-			Member member = session.get(Member.class, 2);
+			Member member = session.get(Member.class, 4);
 			
 			if (member != null) {
 				Info info = member.getInfo();
+				
+				System.out.printf("%s:%s:%s\n", member.getAccount(), member.getPasswd(), member.getName());
+				System.out.printf("%s:%s\n", info.getTel(), info.getBirthday());
+
+				member.setAccount("tony01");
+				member.setPasswd("654321");
+				member.setName("Tony01");
+				
+				info.setTel("7654321");
+				
+				System.out.println("----");
 				System.out.printf("%s:%s:%s\n", member.getAccount(), member.getPasswd(), member.getName());
 				System.out.printf("%s:%s\n", info.getTel(), info.getBirthday());
 				
 				transaction = session.beginTransaction();
-				session.remove(member);
+				session.merge(member);
 				transaction.commit();
-				System.out.println("DELETE OK");
+				System.out.println("Update OK");
 			}else {
 				System.out.println("Not FOUND!");
 			}
@@ -35,6 +46,7 @@ public class Brad03 {
 				transaction.rollback();
 			}
 		}
+
 	}
 
 }
