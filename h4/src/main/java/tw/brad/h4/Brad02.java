78f730acc8b6718b9d7a2593be0df01b1cdccea7
User <User@B01.r1805.tciii.org.tw>
2024-12-17 09:53:43 +0800

    12170953

--- h4/src/main/java/tw/brad/h4/Brad02.java
+++ h4/src/main/java/tw/brad/h4/Brad02.java
@@ -1,9 +1,42 @@
 package tw.brad.h4;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import tw.brad.model.Info;
+import tw.brad.model.Member;
+import tw.brad.utils.HibernateUtil;
+
 public class Brad02 {
 
 	public static void main(String[] args) {
-
+		Transaction transaction = null;
+		
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			transaction = session.beginTransaction();
+			
+			Member member = new Member();
+			member.setAccount("brad04");
+			member.setPasswd("123456");
+			member.setName("Brad04");
+			
+			Info info = new Info();
+			info.setTel("1234567");
+			info.setBirthday("1999-04-02");
+			
+			member.setInfo(info);
+			
+			session.persist(member); 	// 資料庫
+			
+			
+			transaction.commit();
+			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		
+		
+		
 	}
 
 }
