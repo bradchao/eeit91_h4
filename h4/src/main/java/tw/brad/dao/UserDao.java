4af6a35b5878fa1900deb7bc3beaacce4b6d53b0
User <User@B01.r1805.tciii.org.tw>
2024-12-17 11:57:17 +0800

    12171157

---
+++ h4/src/main/java/tw/brad/dao/UserDao.java
@@ -0,0 +1,37 @@
+package tw.brad.dao;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import tw.brad.model.Info;
+import tw.brad.model.Member;
+import tw.brad.model.User;
+import tw.brad.utils.HibernateUtil;
+
+public class UserDao {
+	public void save(User user) {
+		Transaction transaction = null;
+		
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			transaction = session.beginTransaction();
+			
+			session.persist(user);
+			
+			transaction.commit();
+			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		
+	}
+	public void update(User user) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = session.beginTransaction();
+			
+		session.merge(user);
+			
+		transaction.commit();
+		session.close();
+		
+	}
+}
--- h4/src/main/java/tw/brad/h4/Brad07.java
+++ h4/src/main/java/tw/brad/h4/Brad07.java
@@ -3,7 +3,6 @@ package tw.brad.h4;
 public class Brad07 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
