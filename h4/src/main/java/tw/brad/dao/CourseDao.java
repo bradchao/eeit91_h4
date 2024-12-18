d806d4417dc5b2fbac05830cdf0f35c257e1cca7
User <User@B01.r1805.tciii.org.tw>
2024-12-18 09:18:18 +0800

    12180918

--- h4/src/main/java/tw/brad/dao/CourseDao.java
+++ h4/src/main/java/tw/brad/dao/CourseDao.java
@@ -3,27 +3,61 @@ package tw.brad.dao;
 import java.util.List;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import tw.brad.model.Course;
-import tw.brad.model.Student;
 import tw.brad.utils.HibernateUtil;
 
 public class CourseDao {
-	public Course getCourse(int id) {
+	public void add(Course course) {
+		Transaction transaction = null;
+		try(Session session = HibernateUtil.getSessionFactory().openSession()){
+			transaction = session.beginTransaction();
+			session.persist(course);
+			transaction.commit();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	public void update(Course course) {
+		Transaction transaction = null;
+		try(Session session = HibernateUtil.getSessionFactory().openSession()){
+			transaction = session.beginTransaction();
+			session.merge(course);
+			transaction.commit();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void remove(Course course) {
+		Transaction transaction = null;
+		try(Session session = HibernateUtil.getSessionFactory().openSession()){
+			transaction = session.beginTransaction();
+			session.remove(course);
+			transaction.commit();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public Course get(int id) {
 		try(Session session = HibernateUtil.getSessionFactory().openSession()){
 			return session.get(Course.class, id);
 		}catch(Exception e) {
 			System.out.println(e);
 			return null;
 		}
-	}	
-	public List<Course> getAll(){
+	}
+	
+	public List<Course> getAll() {
 		try(Session session = HibernateUtil.getSessionFactory().openSession()){
 			return session.createQuery("FROM Course", Course.class).getResultList();
 		}catch(Exception e) {
 			System.out.println(e);
 			return null;
-		}		
+		}
 	}
 
 	
--- h4/src/main/java/tw/brad/dao/StudentDao.java
+++ h4/src/main/java/tw/brad/dao/StudentDao.java
@@ -1,5 +1,7 @@
 package tw.brad.dao;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -7,16 +9,18 @@ import tw.brad.model.Student;
 import tw.brad.utils.HibernateUtil;
 
 public class StudentDao {
-	public Student getStudent(int id) {
+	public void add(Student student) {
+		Transaction transaction = null;
 		try(Session session = HibernateUtil.getSessionFactory().openSession()){
-			return session.get(Student.class, id);
+			transaction = session.beginTransaction();
+			session.persist(student);
+			transaction.commit();
 		}catch(Exception e) {
 			System.out.println(e);
-			return null;
 		}
 	}
-	
-	public void updateStudent(Student student) {
+
+	public void update(Student student) {
 		Transaction transaction = null;
 		try(Session session = HibernateUtil.getSessionFactory().openSession()){
 			transaction = session.beginTransaction();
@@ -26,4 +30,34 @@ public class StudentDao {
 			System.out.println(e);
 		}
 	}
+	
+	public void remove(Student student) {
+		Transaction transaction = null;
+		try(Session session = HibernateUtil.getSessionFactory().openSession()){
+			transaction = session.beginTransaction();
+			session.remove(student);
+			transaction.commit();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public Student get(int id) {
+		try(Session session = HibernateUtil.getSessionFactory().openSession()){
+			return session.get(Student.class, id);
+		}catch(Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+	
+	public List<Student> getAll() {
+		try(Session session = HibernateUtil.getSessionFactory().openSession()){
+			return session.createQuery("FROM Student", Student.class).getResultList();
+		}catch(Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+	
 }
